70199e652cecbe84679ec1d260f954e2201bbef9
nploi <nploi1998@gmail.com>
2019-04-26 23:20:52 +0700

    Add  agrument for VnExpress

--- news_crawler/spiders/baomoi.py
+++ news_crawler/spiders/baomoi.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import os
+import json
+from codecs import open
 
 URL = 'https://baomoi.com/'
 # Hash table chưa tên chủ đề, để tạo thư mục
@@ -20,36 +22,50 @@ class BaoMoi(scrapy.Spider):
 
     start_urls = [
     ]
+    count = 0
 
     def __init__(self, category=None, *args, **kwargs):
         super(BaoMoi, self).__init__(*args, **kwargs)
         # Tạo thư mục
-        os.mkdir(self.folder_path)
+        if not os.path.exists(self.folder_path):
+            os.mkdir(self.folder_path)
 
         if category in CATEGORYS:
             path = self.folder_path + '/' + CATEGORYS[category];
-            os.makedirs(path)
-            self.start_urls = [URL % category + '.epi']
+            if not os.path.exists(path):
+                os.makedirs(path)   
+            self.start_urls = [URL + category + '.epi']
         else:
             for CATEGORY in CATEGORYS:
-                path = self.folder_path + '/' + CATEGORYS[category];
-                os.makedirs(path)
-                self.start_urls.append(URL % CATEGORY + '.epi');
+                path = self.folder_path + '/' + CATEGORYS[CATEGORY];
+                if not os.path.exists(path):
+                    os.makedirs(path) 
+                self.start_urls.append(URL + CATEGORY + '.epi');
 
     def start_requests(self):
-        print("Comming soon :D")
         for url in self.start_urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        for a in response.css("nav.p_menu a"):
+        items = response.url.split('/')
+        if len(items) >= 4:
+            category = items[3].replace('.epi', '')
+            category = CATEGORYS[category]
+
+        for storyHeading in response.css("div.wrapper.category_page div.main-content div.l-grid__main div.timeline.loadmore div div.story__heading"):
 
             yield {
-                'chu_de': a.css("::text").extract(),
-                'link': a.css("::attr(href)").extract()
+                'title': storyHeading.css("a::text").extract(),
+                'link': response.urljoin(storyHeading.css("a::attr(href)").extract())
             }
             value = {
-                'chu_de': a.css("::text").extract(),
-                'link': a.css("::attr(href)").extract()
+                'title': storyHeading.css("a::text").extract(),
+                'link': response.urljoin(storyHeading.css("a::attr(href)").extract())
             }
-        print("Comming soon :D")
+
+            self.count += 1
+            filename = '%s/%s-%s.txt' % (category, category, self.count)
+            with open(self.folder_path+"/"+filename, 'wb+', encoding= 'utf-8') as fp:
+                json.dump(value, fp, ensure_ascii= False)
+
+            self.log('Saved file %s' % filename)
--- news_crawler/spiders/vnexpress.py
+++ news_crawler/spiders/vnexpress.py
@@ -4,8 +4,8 @@
 import scrapy
 import os
 import json
-import json
 from codecs import open
+from datetime import datetime
 
 URL = 'https://vnexpress.net/'
 
@@ -21,78 +21,93 @@ CATEGORYS = {
 }
 
 class VnExpress(scrapy.Spider):
+    '''Khai thác dữ liệu tin tức từ https://vnexpress.net website
+    ### Các tham số:
+        category: Chủ đề để crawl, có thể bỏ trống. Các chủ đề
+                 * giao-duc
+                 * suc-khoe
+                 * khoa-hoc
+                 * giai-tri
+                 * the-thao
+                 * doi-song
+                 * du-lich  
+        limit: Giới hạn số trang để crawl, có thể bỏ trống.
+    '''
     name = "vnexpress"
     folder_path = "vnexpress"
-    page = "/p"
+    page_limit = None
+    page_count = 0
     count = 0
     start_urls = [
     ]
 
-    def __init__(self, category=None, *args, **kwargs):
+    def __init__(self, category=None, limit=None, *args, **kwargs):
         super(VnExpress, self).__init__(*args, **kwargs)
+        if limit != None:
+            self.page_limit = int(limit)
         # Tạo thư mục
-
-
-        # Create target Directory if don't exist
         if not os.path.exists(self.folder_path):
             os.mkdir(self.folder_path)
 
-
         if category in CATEGORYS:
-            path = self.folder_path + '/' + CATEGORYS[category];
-            if not os.path.exists(path):
-                os.makedirs(path)    
+            folders_path = self.folder_path + '/' + CATEGORYS[category];
+            if not os.path.exists(folders_path):
+                os.makedirs(folders_path)
             self.start_urls = [URL + category]
         else:
             for CATEGORY in CATEGORYS:
-                path = self.folder_path + '/' + CATEGORYS[CATEGORY];
-                if not os.path.exists(path):
-                    os.makedirs(path)  
+                folders_path = self.folder_path + '/' + CATEGORYS[CATEGORY];
+                if not os.path.exists(folders_path):
+                    os.makedirs(folders_path)
                 self.start_urls.append(URL + CATEGORY);
 
     def start_requests(self):
         for url in self.start_urls:
+            self.page_count = 0
             yield scrapy.Request(url=url, callback=self.parse_list_news)
 
     def parse_list_news(self, response):
+        self.page_count = self.page_count + 1
+        if self.page_count > self.page_limit and self.page_limit is not None:
+            return
+
         section = response.css("section section")
         for list_news in section.css("article.list_news"):
             relative_url = list_news.css('h4 a::attr(href)').extract_first()
             abs_url = response.urljoin(relative_url)
-            # print(abs_url)
             yield scrapy.Request(abs_url, callback=self.parse_news)
-        
+
+        # Lấy link trang kế tiếp
         url = response.url;
         next_page_url = section.css("div.pagination.mb10 > a.next::attr(href)").extract_first()
         if "doi-song" in url:
             next_page_url = section.css("div.pagination.mb10 > a.pagination_btn.pa_next.next::attr(href)").extract_first()
         if "du-lich" in url or "giai-tri" in url or "suc-khoe" in url:
             next_page_url = section.css("p.pagination.mb10 > a.next::attr(href)").extract_first()
-
+        
+        # Đệ qui để crawl trang kế tiếp
         if next_page_url is not None:
             yield scrapy.Request(response.urljoin(next_page_url), callback=self.parse_list_news)
 
     def parse_news(self, response):
         news = response.css("section section section");
-        items = response.url.split('/')
-
-        yield {
-            'title': news.css("h1::text").extract(),
-            'content': news.css("article p::text").getall(),
-            'link': response.url,
-        }
 
         jsonData = {
-            'title': news.css("h1::text").extract(),
-            'content': news.css("article p::text").getall(),
+            'date': news.css("header span::text").extract_first(),
+            'title': news.css("h1::text").extract_first(),
             'link': response.url,
+            'content': news.css("article p::text").getall(),
         }
 
+        yield jsonData
+
+        items = response.url.split('/')
+
+        # Write to file
         if len(items) >= 5 and items[3] in CATEGORYS:
             self.count += 1
-            filename = '%s/%s-%s.txt' % (CATEGORYS[items[3]], CATEGORYS[items[3]], self.count)
-            with open(self.folder_path+"/"+filename, 'wb+', encoding= 'utf-8') as fp:
+            filename = '%s/%s-%s.json' % (CATEGORYS[items[3]], CATEGORYS[items[3]], self.count)
+            with open(self.folder_path+"/"+filename, 'wb', encoding= 'utf-8') as fp:
                 json.dump(jsonData, fp, ensure_ascii= False)
-
             self.log('Saved file %s' % filename)
         
