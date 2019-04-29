eff7e61a80f96cfdb136cd09bb0bacf2199a2b6b
nploi <nploi1998@gmail.com>
2019-04-29 16:32:50 +0700

    Add  to write json with unicode

--- news_crawler/settings.py
+++ news_crawler/settings.py
@@ -88,3 +88,5 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+FEED_EXPORT_ENCODING = 'utf-8'
